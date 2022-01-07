b4bf0884d0f1e6cfc8dd307d7d42be3a3ddc83c0
arizalpratama <arizalpratama01@gmail.com>
2022-01-08 01:33:27 +0700

    add applicationDb context

---
+++ EnrollmentService/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EnrollmentService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnrollmentService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //DbContext
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+    }
+}
