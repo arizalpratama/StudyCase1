ece952c37836a3f769f79a1ece8f3b8dcf27697b
arizalpratama <arizalpratama01@gmail.com>
2022-01-08 03:43:28 +0700

    add student DAL

--- EnrollmentService/DAL/CourseDAL.cs
+++ EnrollmentService/DAL/CourseDAL.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using EnrollmentService.Dtos;
 using EnrollmentService.Models;
 using System;
 using System.Collections.Generic;
--- EnrollmentService/DAL/StudentDAL.cs
+++ EnrollmentService/DAL/StudentDAL.cs
@@ -1,6 +1,90 @@
-﻿namespace EnrollmentService.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using EnrollmentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnrollmentService.Data;
+using EnrollmentService.Interface;
+
+namespace EnrollmentService.DAL
 {
-    public class StudentDAL
+    public class StudentDAL : IStudent
     {
+        private ApplicationDbContext _db;
+        public StudentDAL(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Delete
+        public async Task Delete(string id)
+        {
+            var result = await GetById(id);
+            if (result == null) throw new Exception("Data tidak ditemukan !");
+            try
+            {
+                _db.Students.Remove(result);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbEx)
+            {
+
+                throw new Exception($"Error: {dbEx.Message}");
+            }
+        }
+
+        //Get All
+        public async Task<IEnumerable<Student>> GetAll()
+        {
+            var results = await (from s in _db.Students
+                                 orderby s.FirstName ascending
+                                 select s).ToListAsync();
+            return results;
+        }
+
+        public async Task<Student> GetById(string id)
+        {
+            var result = await _db.Students.Where
+                (s => s.Id == Convert.ToInt32(id)).SingleOrDefaultAsync<Student>();
+            if (result != null)
+                return result;
+            else
+                throw new Exception("Data tidak ditemukan !");
+        }
+
+        //Insert
+        public async Task<Student> Insert(Student obj)
+        {
+            try
+            {
+                _db.Students.Add(obj);
+                await _db.SaveChangesAsync();
+                return obj;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
+        }
+
+        //Update
+        public async Task<Student> Update(string id, Student obj)
+        {
+            try
+            {
+                var result = await GetById(id);
+                result.FirstName = obj.FirstName;
+                result.LastName = obj.LastName;
+                result.EnrollmentDate = obj.EnrollmentDate;
+                await _db.SaveChangesAsync();
+                obj.Id = Convert.ToInt32(id);
+                return obj;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Error: {dbEx.Message}");
+            }
+        }
     }
 }
