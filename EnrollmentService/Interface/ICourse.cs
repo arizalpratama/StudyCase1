da942a4763333d1620fc6a63abcf09edced99329
arizalpratama <arizalpratama01@gmail.com>
2022-01-08 02:45:48 +0700

    add interface

---
+++ EnrollmentService/Interface/ICourse.cs
@@ -0,0 +1,8 @@
+﻿using EnrollmentService.Models;
+
+namespace EnrollmentService.Interface
+{
+    public interface ICourse : ICrud<Course>
+    {
+    }
+}
---
+++ EnrollmentService/Interface/IEnrollment.cs
@@ -0,0 +1,8 @@
+﻿using EnrollmentService.Models;
+
+namespace EnrollmentService.Interface
+{
+    public interface IEnrollment : ICrud<Enrollment>
+    {
+    }
+}
