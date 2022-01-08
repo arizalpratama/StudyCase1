4a3688414acde8a93c2c70c32a1416ffb8c7223b
arizalpratama <arizalpratama01@gmail.com>
2022-01-08 23:48:19 +0700

    add enrollments controller

---
+++ PaymentService/Controllers/EnrollmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PaymentService.Controllers
+{
+    [Route("api/p/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        public EnrollmentsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestIndboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST command services");
+            return Ok("Inbound test from platforms controller");
+        }
+    }
+}
