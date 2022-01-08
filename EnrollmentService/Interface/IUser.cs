c24e5e85ed270a6591c4b1f3ee0453282a1c95dd
arizalpratama <arizalpratama01@gmail.com>
2022-01-08 22:36:46 +0700

    add interface user

---
+++ EnrollmentService/Interface/IUser.cs
@@ -0,0 +1,31 @@
+﻿using EnrollmentService.Dtos;
+using EnrollmentService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EnrollmentService.Interface
+{
+    public interface IUser
+    {
+        //Get All
+        IEnumerable<UserDto> GetAllUser();
+
+        //Registration
+        Task Registration(CreateUserDto user);
+
+        //Add Roles
+        Task AddRole(string rolename);
+
+        //Get Roles
+        IEnumerable<CreateRoleDto> GetRoles();
+
+        //Add User To Roles
+        Task AddUserToRole(string username, string role);
+
+        //Get Roles From User
+        Task<List<string>> GetRolesFromUser(string username);
+
+        //Authentication
+        Task<User> Authenticate(string username, string password);
+    }
+}
