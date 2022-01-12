823bb0322dd71c0a85848303861c8d8e5ff6348b
arizalpratama <arizalpratama01@gmail.com>
2022-01-12 15:36:12 +0700

    add http payment data client

--- EnrollmentService/Startup.cs
+++ EnrollmentService/Startup.cs
@@ -128,8 +128,6 @@ namespace EnrollmentService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EnrollmentService v1"));
             }
 
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthentication();
