50f6b42296b83cdcd2ceb069a7395727cb4f5ae3
arizalpratama <arizalpratama01@gmail.com>
2022-01-12 15:32:28 +0700

    add enrollment interface

--- EnrollmentService/Interface/IEnrollment.cs
+++ EnrollmentService/Interface/IEnrollment.cs
@@ -13,21 +13,3 @@ namespace EnrollmentService.Data
         Task DeleteEnrollment(string id);
     }
 }
-
-/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EnrollmentService.Models;
-
-namespace EnrollmentService.Interface
-{
-    public interface IEnrollment
-    {
-        bool SaveChanges();
-        IEnumerable<Enrollment> GetAllEnrollments();
-        Enrollment GetEnrollmentById(int id);
-        void CreateEnrollment(Enrollment enrol);
-        Task Delete(string id);
-    }
-}*/
